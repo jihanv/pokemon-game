85042e91f40e7a0978f96e1981d0aeb032a1bbf4
jihanv <hedjmny@gmail.com>
2026-01-04 21:40:18 +0900

    Create collisionsMap. TODO turn it into a canvas object.

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
-
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, i + 70));
+}
 const image = new Image();
 image.src = "./images/Pellet Town.png";
 
