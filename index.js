09d2280576f71318f9c7638ee4396be1515b36b9
jihanv <hedjmny@gmail.com>
2026-01-08 23:17:49 +0900

    Refactor classes into a separate file.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//2:41
+//3:05
 const canvas = document.querySelector("canvas");
 
 const c = canvas.getContext("2d");
@@ -10,20 +10,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, i + 70));
 }
 
-class Boundary {
-  static width = 48;
-  static height = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48;
-    this.height = 48;
-  }
-
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
 const offset = {
   x: -735,
   y: -640,
@@ -51,36 +37,6 @@ image.src = "./images/Pellet Town.png";
 
 const playerImage = new Image();
 playerImage.src = "./images/playerDown.png";
-
-class Sprite {
-  constructor({ position, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-    };
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      // canvas.width / 2 - this.image.width / 8,
-      // canvas.height / 2 - this.image.height / 2,
-      // this.image.width / 4,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
